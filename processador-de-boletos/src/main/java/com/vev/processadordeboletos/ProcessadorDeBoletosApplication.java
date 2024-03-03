419dabd8c6515e866469a51d98826ec07c266d9c
Crisley Venâncio Marques <crisley.marques@ccc.ufcg.edu.br>
2024-03-03 19:52:00 -0300

    inicializa projetos spring

---
+++ processador-de-boletos/src/main/java/com/vev/processadordeboletos/ProcessadorDeBoletosApplication.java
@@ -0,0 +1,13 @@
+package com.vev.processadordeboletos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessadorDeBoletosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessadorDeBoletosApplication.class, args);
+	}
+
+}
---
+++ processador-de-boletos/src/test/java/com/vev/processadordeboletos/ProcessadorDeBoletosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vev.processadordeboletos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessadorDeBoletosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
